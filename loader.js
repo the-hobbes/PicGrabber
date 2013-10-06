fe0da9f23e3f082dbe2f98c8d5ed2635b754ecef
the-hobbes <phelan.vendeville@gmail.com>
2013-10-06 12:14:47 -0700

    cleaned up code, removing chrome.downloads as it only works in dev/beta channels of chrome. Instead, we will be saving things to dropbox with the saver api. Also, getting ready to finish the image size checker step.

--- loader.js
+++ loader.js
@@ -1,6 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
-  console.log('Running pic grabber against: ' + tab.url + ' now!');
   chrome.tabs.executeScript({
     file: "grabber.js"
     });
